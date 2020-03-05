713abbbe294bb1e41a39b04a94bfd2fdeae3dab5
Jack12xl <jack12xieling@163.com>
2020-03-05 19:28:59 +0800

    add yolo change , currently config would trigger segement fault

--- mmdet/models/utils/__init__.py
+++ mmdet/models/utils/__init__.py
@@ -5,9 +5,9 @@ from .scale import Scale
 from .upsample import build_upsample_layer
 from .weight_init import (bias_init_with_prob, kaiming_init, normal_init,
                           uniform_init, xavier_init)
-
+from .yolo_utils import YoloConvLayer
 __all__ = [
     'conv_ws_2d', 'ConvWS2d', 'build_conv_layer', 'ConvModule',
     'build_norm_layer', 'build_upsample_layer', 'xavier_init', 'normal_init',
-    'uniform_init', 'kaiming_init', 'bias_init_with_prob', 'Scale'
+    'uniform_init', 'kaiming_init', 'bias_init_with_prob', 'Scale', 'YoloConvLayer'
 ]
